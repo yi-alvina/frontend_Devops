1d95fca7c00bdc0e101529f7f5ce9848b9f82a6b
Yileanne <166559952+yi-alvina@users.noreply.github.com>
2026-05-28 20:31:28 -0400

    Update Navbar title to include ':D'

--- front_despacho/src/componentes/Layouts/Navbar.jsx
+++ front_despacho/src/componentes/Layouts/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
-      <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
+      <h2 className="text-xl font-bold mb-8">Despacho Dashboard :D</h2>
 
       {/* Menú de navegación */}
       <ul className="space-y-3">
